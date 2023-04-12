a1e58b7e1e92766c89acd526dfe7030c760f5ea7
AmbivelenS <45044656+AmbivelenS@users.noreply.github.com>
2023-04-12 09:26:39 -0700

    Add files via upload

---
+++ Intervention_21319/BST.lua
@@ -0,0 +1,195 @@
+local profile = {};
+gcdisplay = gFunc.LoadFile('common\\gcdisplay.lua');
+gcinclude = gFunc.LoadFile('common\\gcinclude.lua');
+profile.Sets = {
+    Idle = {
+        Main = 'Viking Axe',
+        Sub = 'Viking Axe',
+        Head = 'Mrc.Cpt. Headgear',
+        Neck = 'Peacock Amulet',
+        Ear1 = 'Beetle Earring +1',
+        Ear2 = 'Beetle Earring +1',
+        Body = 'Mrc.Cpt. Doublet',
+        Hands = 'Battle Gloves',
+        Ring1 = 'Balance Ring',
+        Ring2 = 'Balance Ring',
+        Waist = 'Life Belt',
+        Legs = 'Republic Subligar',
+        Feet = 'Wonder Clomps',
+    },
+	Idle_Regen = {
+	},
+	Idle_Refresh = {
+	},
+	Tp_Default = { --Haste Gear
+        Head = 'Mrc.Cpt. Headgear',
+        Neck = 'Peacock Amulet',
+        Ear1 = 'Beetle Earring +1',
+        Ear2 = 'Beetle Earring +1',
+        Body = 'Mrc.Cpt. Doublet',
+        Hands = 'Battle Gloves',
+        Ring1 = 'Courage Ring',
+        Ring2 = 'Courage Ring',
+        Waist = 'Life Belt',
+        Legs = 'Republic Subligar',
+        Feet = 'Wonder Clomps',
+    },
+	Tp_Hybrid ={ --STR / Haste / ACC
+        Neck = 'Spike Necklace',
+        Hands = 'Wonder Mitts',
+        Feet = 'Wonder Clomps',
+	},
+	Tp_Acc={ --ACC
+        Hands = 'Battle Gloves',
+		Ring1 = 'Balance Ring',
+        Ring2 = 'Balance Ring',
+
+	
+	},
+
+	Ws_Default = {
+        Head = 'Mrc.Cpt. Headgear',
+        Neck = 'Spike Necklace',
+        Ear1 = 'Beetle Earring +1',
+        Ear2 = 'Beetle Earring +1',
+        Body = 'Wonder Kaftan',
+        Hands = 'Wonder Mitts',
+        Ring1 = 'Courage Ring',
+        Ring2 = 'Courage Ring',
+        Waist = 'Life Belt',
+        Legs = 'Wonder Braccae',
+        Feet = 'Wonder Clomps',
+	},
+	Ws_Hybrid = {
+    },
+	Ws_Acc = {
+		Neck = 'Peacock Amulet',
+	},
+	Dt = {
+	},
+	Movement = {
+	},
+	Pet_Dt = {
+	},
+    HP = {
+        Body = 'Wonder Kaftan',
+        Hands = 'Wonder Mitts',
+        Back = 'Breath Mantle',
+        Legs = 'Wonder Braccae',
+        Feet = 'Wonder Clomps',
+    },
+	Charm = {
+		Head = 'Noble\'s Ribbon',
+		Neck = 'Bird Whistle',
+        Ring1 = 'Hope Ring',
+        Ring2 = 'Hope Ring',
+		Waist = 'Corsette',
+
+    },
+	
+};
+
+
+
+profile.Packer = {
+};
+
+
+
+profile.OnLoad = function()
+	gcinclude.Initialize();
+    gSettings.AllowAddSet = true;
+	AshitaCore:GetChatManager():QueueCommand(1, '/macro book 4');
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1');
+	gcinclude.settings.RegenGearHPP = 0;
+    gcinclude.settings.RefreshGearMPP = 0;
+end
+
+profile.OnUnload = function()
+	gcinclude.Unload();
+end
+
+profile.HandleCommand = function(args)
+    gcinclude.SetCommands(args);
+end
+	
+profile.HandleDefault = function()
+	gFunc.EquipSet(profile.Sets.Idle);
+	local test = gcinclude.levelSync(55)
+	--print(test)
+	local player = gData.GetPlayer();
+    if (player.Status == 'Engaged') then
+        gFunc.EquipSet(profile.Sets.Tp_Default)
+        if (gcdisplay.GetCycle('MeleeSet') ~= 'Default') then
+			gFunc.EquipSet('Tp_' .. gcdisplay.GetCycle('MeleeSet')) end
+		--if (gcdisplay.GetToggle('TH') == true) then gFunc.EquipSet(profile.Sets.TH) end
+    elseif (player.Status == 'Resting') then
+        gFunc.EquipSet(profile.Sets.Resting);
+    elseif (player.IsMoving == true) then
+		gFunc.EquipSet(profile.Sets.Movement);
+    end
+	
+    gcinclude.CheckDefault();
+    --if (gcdisplay.GetToggle('DTset') == true) then gFunc.EquipSet(profile.Sets.Dt) end;
+    --if (gcdisplay.GetToggle('Kite') == true) then gFunc.EquipSet(profile.Sets.Movement) end;
+end
+
+profile.HandleWeaponskill = function()
+    local canWS = gcinclude.CheckWsBailout();
+    if (canWS == false) then gFunc.CancelAction() return;
+    else
+        local ws = gData.GetAction();
+    
+        gFunc.EquipSet(profile.Sets.Ws_Default)
+	if (gcdisplay.GetCycle('MeleeSet') ~= 'Default') then
+        gFunc.EquipSet('Ws_' .. gcdisplay.GetCycle('MeleeSet')) end
+        
+--        if string.match(ws.Name, 'Aeolian Edge') then
+--            gFunc.EquipSet(profile.Sets.Aedge_Default)
+--            if (gcdisplay.GetCycle('MeleeSet') ~= 'Default') then
+--            gFunc.EquipSet('Aedge_' .. gcdisplay.GetCycle('MeleeSet')); end
+--            if (gcdisplay.GetCycle('MeleeSet') == 'Default') then gcinclude.DoMoonshade() end;
+--        elseif string.match(ws.Name, 'Savage Blade') then
+--            gFunc.EquipSet(profile.Sets.Savage_Default)
+--            if (gcdisplay.GetCycle('MeleeSet') ~= 'Default') then
+--            gFunc.EquipSet('Savage_' .. gcdisplay.GetCycle('MeleeSet')); end
+ --           if (gcdisplay.GetCycle('MeleeSet') == 'Default') then gcinclude.DoMoonshade() end;
+
+	end
+end
+profile.HandleAbility = function()
+    local ability = gData.GetAction();
+	if string.match(ability.Name, 'Charm') then
+		gFunc.EquipSet(profile.Sets.Charm); end
+end
+
+profile.HandleItem = function()
+end
+
+profile.HandlePrecast = function()
+end
+
+profile.HandleMidcast = function()
+    local spell = gData.GetAction();
+
+    if (spell.Skill == 'Ninjutsu') then
+        if string.contains(spell.Name, 'Utsusemi') then
+            gFunc.EquipSet(profile.Sets.Nuke);
+			--gFunc.EquipSet(profile.Sets.Utsu);
+        elseif (gcinclude.NinNukes:contains(spell.Name)) then
+            gFunc.EquipSet(profile.Sets.Nuke);
+        else
+			gFunc.EquipSet(profile.Sets.Nuke);
+            --gFunc.EquipSet(profile.Sets.Macc);
+        end
+    end
+end
+
+profile.HandlePreshot = function()
+end
+
+profile.HandleMidshot = function()
+end
+
+
+return profile;
