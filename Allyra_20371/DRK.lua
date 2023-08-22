f4ffeaf369443babb2733f8bc0022e102cb5ed96
AmbivelenS <45044656+AmbivelenS@users.noreply.github.com>
2023-08-21 19:18:43 -0700

    Update DRK.lua
    cleaned up

--- Allyra_20371/DRK.lua
+++ Allyra_20371/DRK.lua
@@ -25,16 +25,13 @@ layers.Sets.PDT = {
 
 layers.Sets.Refresh = {
         Neck = 'Parade Gorget',
-
-}
+    }
 
 layers.Sets.Idle = {
-        Ammo = 'Happy Egg',
-        Head = 'Optical Hat',
         Neck = 'Parade Gorget',
         Ear1 = 'Coral Earring',
         Ear2 = 'Optical Earring',
-        Body = 'Scorpion Harness',
+        Body = 'Vampire Cloak',
         Hands = 'Thick Mufflers',
         Ring1 = 'Rajas Ring',
         Ring2 = 'Jaeger Ring',
@@ -45,155 +42,50 @@ layers.Sets.Idle = {
     }
 
 layers.Sets.Engaged = {
-        Ammo = 'Happy Egg',
         Head = 'Optical Hat',
         Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
         Body = 'Scorpion Harness',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
-        Legs = 'Black Cuisses',
-        Feet = 'Homam Gambieras',
     }
 
 layers.Sets.Weaponskill ={
-        Ammo = 'Happy Egg',
-        Head = 'Optical Hat',
         Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
         Body = 'Scorpion Harness',
         Hands = 'Thick Mufflers',
-        Ring1 = 'Sun Ring',
         Ring2 = 'Sun Ring',
-        Back = 'Abyss Cape',
         Waist = 'Life Belt',
-        Legs = 'Black Cuisses',
-        Feet = 'Homam Gambieras',
-}
+    }
 
 layers.Sets.Ability['Souleater'] = {
-        Ammo = 'Happy Egg',
         Head = 'Chaos Burgeonet',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
         Body = 'Gloom Breastplate',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
-        Legs = 'Black Cuisses',
-        Feet = 'Homam Gambieras',
     }
-layers.Sets.Ability['WeaponBash'] = {
-        Ammo = 'Happy Egg',
-        Head = 'Optical Hat',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
-        Body = 'Scorpion Harness',
+layers.Sets.Ability['Weapon Bash'] = {
         Hands = 'Chaos Gauntlets',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
-        Legs = 'Black Cuisses',
-        Feet = 'Homam Gambieras',
-}
-layers.Sets.Ability['Reward'] ={
-        Body = "Monster Jackcoat",
-        Hands = 'Ogre Gloves',
-        Feet = 'Beast Gaiters',
-        Neck = 'Justice Badge',
-        Ring1 = 'Saintly Ring',
-        Ring2 = 'Saintly Ring',
-}
+    }
 
-
-layers.Sets.Ability['ArcaneCircle'] = {
-        Ammo = 'Happy Egg',
-        Head = 'Optical Hat',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
-        Body = 'Scorpion Harness',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
-        Legs = 'Black Cuisses',
+layers.Sets.Ability['Arcane Circle'] = {
         Feet = 'Chaos Sollerets',
     }
-
+layers.Sets.Ability['Last Resort'] = {
+        Feet = 'Abyss Sollerets', 
+    }
 
 layers.Sets.Dark = {
-        Ammo = 'Happy Egg',
         Head = 'Chaos Burgeonet',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
-        Body = 'Scorpion Harness',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
         Legs = 'Abyss Flanchard',
-        Feet = 'Homam Gambieras',
     }
 layers.Sets.Elemental = {
-        Ammo = 'Happy Egg',
-        Head = 'Optical Hat',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
-        Body = 'Scorpion Harness',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
-        Legs = 'Black Cuisses',
-        Feet = 'Homam Gambieras',
+
     }
 
 layers.Sets.Enfeebling = {
-        Ammo = 'Happy Egg',
-        Head = 'Optical Hat',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
         Body = 'Chaos Cuirass',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
-        Legs = 'Abyss Flanchard',
         Feet = 'Abyss Sollerets',
     }
 
 layers.Sets.AbsorbStat = {
-        Ammo = 'Happy Egg',
         Head = 'Chaos Burgeonet',
-        Neck = 'Peacock Amulet',
-        Ear1 = 'Coral Earring',
-        Ear2 = 'Optical Earring',
-        Body = 'Scorpion Harness',
-        Hands = 'Thick Mufflers',
-        Ring1 = 'Rajas Ring',
-        Ring2 = 'Jaeger Ring',
-        Back = 'Abyss Cape',
-        Waist = 'Swift Belt',
         Legs = 'Black Cuisses',
-        Feet = 'Homam Gambieras',
     }
 
 layers.Sets.Midcast['Dark Affinity'] = layers.Sets.Dark
